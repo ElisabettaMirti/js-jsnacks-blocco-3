c7bad7e8889d939dc249eed501c9c8c4ae1810f5
ElisabettaMirti <belmirti@gmail.com>
2024-05-15 11:57:34 +0200

    inserito snack9

---
+++ snack9/js/script.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto. Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
+//  se è dispari inseriscilo nell'array.
+
+
+const oddArray = [];
+
+let userNumber;
+
+while (oddArray.lenght < 6) {
+    userNumber = Number.parseInt(prompt('Type a number'));
+    if (userNumber % 2 === 1) {
+        oddArray.push(userNumber);
+    }
+}
