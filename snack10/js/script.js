75f0ecb0568fbb8ae4abe063eb8fb2398162981b
ElisabettaMirti <belmirti@gmail.com>
2024-05-15 12:36:00 +0200

    correzioni

--- snack10/js/script.js
+++ snack10/js/script.js
@@ -1,26 +1,13 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const arrayA = [1, 2, 3, 4];
-const arrayB = [5, 6, 7, 8, 9, 10, 11, 12];
+let arrayCorto = [1, 2, 3, 4];
+let arrayLungo = [5, 6, 7, 8, 9, 10, 11, 12];
 
-let arrayLungo;
-let arrayCorto;
 
-if (arrayA.lenght > arrayB.lenght) {
-    arrayLungo = arrayA;
-    arrayCorto = arrayB;
-} else if (arrayA.lenght < arrayB.lenght) {
-    arrayLungo = arrayB;
-    arrayCorto = arrayA;
-} else {
-    console.log('Hanno la stessa lunghezza')
-}
 
-console.log(arrayLungo, arrayCorto);
-
-while (arrayCorto.lenght = arrayLungo.lenght) {
-    arrayCorto.push(Math.floor(Math.random() * 100)+ 1);
+while (arrayCorto.length = arrayLungo.length) {
+    arrayCorto.push(1);
 }
 
 console.log(arrayLungo, arrayCorto);
