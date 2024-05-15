e348e161dfaa237340e3f78cc8b3ce82efb8f9f2
ElisabettaMirti <belmirti@gmail.com>
2024-05-15 12:16:58 +0200

    inserite condizioni per verificare la lunghezza dei due array

---
+++ snack10/js/script.js
@@ -0,0 +1,19 @@
+// Crea due array che hanno un numero di elementi diversi. 
+// Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+const arrayA = [1, 2, 3, 4];
+const arrayB = [5, 6, 7, 8, 9, 10, 11, 12];
+
+let arrayLungo;
+let arrayCorto;
+
+if (arrayA.lenght > arrayB.lenght) {
+    arrayLungo = arrayA;
+    arrayCorto = arrayB;
+} else if (arrayA.lenght < arrayB.lenght) {
+    arrayLungo = arrayB;
+    arrayCorto = arrayA;
+} else {
+    console.log('Hanno la stessa lunghezza')
+}
+
