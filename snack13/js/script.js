eac520af8c8e03b7688262e03de63ee48f24dafb
ElisabettaMirti <belmirti@gmail.com>
2024-05-16 12:12:24 +0200

    inizio snack13

---
+++ snack13/js/script.js
@@ -0,0 +1,11 @@
+// > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
+
+const userNumber = Number.parseInt(prompt('Type a number'), 10)
+
+
+function reverseNumber (anyNumber) {
+
+    for ( i = anyNumber.length; i > 0 ; i = i - 1){
+
+    }
+}
