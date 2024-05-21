48be19b1f69c2e430ddb28e8e544791c696d5751
ElisabettaMirti <belmirti@gmail.com>
2024-05-21 12:29:32 +0200

    snack14

---
+++ snack14/js/script.js
@@ -0,0 +1,10 @@
+// Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
+
+const userTime = Number.parseInt(prompt('Quanti secondi ti servono?'));
+
+setTimeout (timeIsOver, userTime)
+
+function timeIsOver() {
+    alert('Fine tempo!');
+}
+
