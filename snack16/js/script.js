31d63e846ef58bbd4c2f99abdbdbe4d9bb526240
ElisabettaMirti <belmirti@gmail.com>
2024-05-22 12:42:22 +0200

    snack16

---
+++ snack16/js/script.js
@@ -0,0 +1,14 @@
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+
+let palla = {
+    nome: "palla",
+    peso: 10
+};
+
+console.log(palla);
+
+palla.peso = Number.parseInt(prompt('Scrivi il peso della palla'));
+console.log(palla);
