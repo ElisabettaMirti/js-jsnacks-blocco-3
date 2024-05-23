93f854475a60d00c9eabe454843c5426e1b18832
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 12:14:52 +0200

    snack js object

---
+++ snack-jsObject-1/js/script.js
@@ -0,0 +1,90 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+const Automobili = [
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'metano'
+    }
+];
+
+const aBenzina = Automobili.filter((auto) => {
+    if (Automobili.alimentazione = 'benzina'){
+        return true;
+    }
+});
+
+console.log(aBenzina);
